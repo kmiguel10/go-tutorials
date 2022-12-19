0e0539a57002b70fa1a5c2defa10c11e2298d4a4
Kent Miguel <kentmiguel10@gmail.com>
2022-12-19 21:41:56 +0100

    Return and handle error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,12 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person
 // In Go, a function that start with a capital letter can be called by a function in the same package. Also known as exported name
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	//if no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	//return a greeting that embeds the name in a message
 	// := declared and initializes a variable
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
